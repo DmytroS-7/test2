09063bd38eebe005074090dd52d9c5eeb381deca
DmytroS-7 <dm.webmas12@gmail.com>
2020-02-22 17:45:23 +0200

    add object user

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,20 @@
+const user = {
+  firstname: "John",
+  lastname: "Connor",
+  age: 30,
+  date: "10.10.2000",
+  height: 186,
+  animals: false,
+  food: ["soup", "pizza", "fruit"],
+  getNameDate: function() {
+    console.log(this.firstname + " " + this.lastname + " " + this.date);
+  }
+};
+
+user.getNameDate();
+console.log(user.food[0]);
+console.log("-------------------------------");
+
 const a = 10;
 const b = 5;
 console.log("a =", a);
